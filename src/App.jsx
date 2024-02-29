8fcb8d637ed6f2302d3374fef7f535aec54f3e87
Akhil <akhilbolla89@gmail.com>
2024-02-29 17:09:40 +0530

    p1

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header/Header"
+
+import "./App.css"
+
+const App =() => {
+  return(
+    <>
+    <Header/>
+    <main>
+      {/* <Home/>
+      <About/>
+      <Skills/>
+      <Services/>
+      <Qualification/>
+      <Portfolio />
+      <Contact /> */}
+    </main>
+    {/* <Footer />
+    <ScrollUp /> */}
+    </>
+  )
+}
+
+export default App
